5cedd9d7e9cd5148b01d57bd6d9335af7520d95b
richfig683 <richfig683@gmail.com>
2021-05-31 13:40:52 -0400

    build the bins page UI

--- src/pages/bins/bins.js
+++ src/pages/bins/bins.js
@@ -5,44 +5,169 @@ import { Picker } from '@react-native-picker/picker';
 
 import styles from "./styles";
 
-class Bins extends Component {
-    constructor(props) {
-        super(props);
-    }
+const Bins = (props) => {
+    const defaultStatus = [
+        {
+            number: "1",
+            type: "One"
+        },
+        {
+            number: "2",
+            type: "Clean"
+        },
+        {
+            number: "3",
+            type: "Repair"
+        },
+        {
+            number: "4",
+            type: "Retire"
+        },
+        {
+            number: "5",
+            type: "Five"
+        },
+        {
+            number: "6",
+            type: "Six"
+        },
+        {
+            number: "7",
+            type: "Seven"
+        },
+        {
+            number: "8",
+            type: "Eight"
+        },
+    ];
+    const defaultBins = [
+        {
+            type: "A",
+            number1: "100",
+            number2: "000001",
+            number3: 72
+        },
+        {
+            type: "B",
+            number1: "100",
+            number2: "000001",
+            number3: 72
+        },
+        {
+            type: "C",
+            number1: "100",
+            number2: "000001",
+            number3: 72
+        },
+        {
+            type: "D",
+            number1: "100",
+            number2: "000001",
+            number3: 72
+        },
+        {
+            type: "E",
+            number1: "100",
+            number2: "000001",
+            number3: 72
+        },
+        {
+            type: "F",
+            number1: "100",
+            number2: "000001",
+            number3: 72
+        },
+        {
+            type: "G",
+            number1: "100",
+            number2: "000001",
+            number3: 72
+        },
+        {
+            type: "H",
+            number1: "100",
+            number2: "000001",
+            number3: 72
+        }
+    ];
 
-    selectStatus = (itemValue) => {
-        console.log('itemValue');
-    }
+    const [statuses, setStatuses] = React.useState(defaultStatus);
+    const [status, setStatus] = React.useState(statuses[0]);
+    const [bins, setBins] = React.useState(defaultBins);
 
-    render() {
-        return (
-            <ScrollView>
-                <View style={styles.container}>
-                    <View style={styles.formContainer}>
-                        <Text style={styles.label}>Name</Text>
-                        <TextInput style={styles.input} placeholder="XXXX XXXXX" />
-                    </View>
-                    <View style={styles.formContainer}>
-                        <Text style={styles.label}>Number</Text>
-                        <TextInput style={styles.input} placeholder="123456789" />
-                    </View>
-                    <View style={styles.formContainer}>
-                        <Text style={styles.label}>Status</Text>
-                        <Picker
-                            selectedValue="1"
-                            onValueChange={(itemValue, itemIndex) =>
-                                selectStatus(itemValue)
-                            }
-                        >
-                            <Picker.Item label="1" value="1" />
-                            <Picker.Item label="2" value="2" />
-                        </Picker>
-                        <TextInput style={styles.statusInput} placeholder="123456789" />
+    return (
+        <ScrollView>
+            <View style={styles.container}>
+                <View style={styles.formContainer}>
+                    <Text style={styles.label}>Name</Text>
+                    <TextInput style={styles.input} placeholder="XXXX XXXXX" />
+                </View>
+                <View style={styles.formContainer}>
+                    <Text style={styles.label}>Number</Text>
+                    <TextInput style={styles.input} placeholder="123456789" />
+                </View>
+                <View style={styles.formContainer}>
+                    <Text style={styles.label}>Status</Text>
+                    <Picker
+                        selectedValue={status}
+                        style={{
+                            width: 120
+                        }}
+                        onValueChange={(itemValue, itemIndex) => setStatus(itemValue)}
+                    >
+                        {
+                            statuses.map((status, key) => {
+                                return (
+                                    <Picker.Item key={key} label={status.number} value={status} />
+                                )
+                            })
+                        }
+                    </Picker>
+                    <TextInput style={styles.statusInput} value={status.type} />
+                </View>
+                <View style={styles.formContainer}>
+                    <Text style={styles.label}></Text>
+                    <View style={styles.addButtonContainer}>
+                        <Button style={styles.addButton}>
+                            <Text style={styles.addButtonText}>Add</Text>
+                        </Button>
                     </View>
                 </View>
-            </ScrollView>
-        );
-    }
+                <View style={styles.formContainer}>
+                    <View style={styles.binsLabelView}>
+                        <Text style={styles.label}>Bin</Text>
+                    </View>
+                    <View style={styles.binsView}>
+                        {
+                            bins.map((bin, key) => {
+                                return (
+                                    <View key={key} style={styles.binView}>
+                                        <Text>{bin.type}</Text>
+                                        <TextInput style={styles.binInput} value={bin.number1} />
+                                        <TextInput style={styles.binInput} value={bin.number2} />
+                                        <Text>{bin.number3}</Text>
+                                    </View>
+                                )
+                            })
+                        }
+                    </View>
+                </View>
+
+                <View style={styles.formContainer}>
+                    <Text style={styles.binAssignmentText}>Bin Assignment</Text>
+                    <TextInput style={styles.binAssignmentInput} />
+                </View>
+                <View style={styles.formContainer}>
+                    <Text style={styles.binAssignmentText}>Customer</Text>
+                    <TextInput style={styles.binAssignmentInput} />
+                </View>
+                <View style={styles.formContainer}>
+                    <Text style={styles.binAssignmentText}>Addreess</Text>
+                    <TextInput style={styles.binAssignmentInput} />
+                </View>
+            </View>
+        </ScrollView>
+    );
 }
 
 export default Bins;
--- src/pages/bins/styles.js
+++ src/pages/bins/styles.js
@@ -5,7 +5,8 @@ export default StyleSheet.create({
     paddingTop: 40,
     paddingLeft: 15,
     paddingRight: 15,
-    backgroundColor: '#fff'
+    backgroundColor: '#fff',
+    marginBottom: 120
   },
   formContainer: {
     flexDirection: "row",
@@ -19,12 +20,63 @@ export default StyleSheet.create({
     marginRight: 5
   },
   input: {
-    flex: 3,
+    flex: 4,
     height: 40,
     borderWidth: 1
   },
   statusInput: {
     flex: 2,
+    height: 40,
+    borderWidth: 1
+  },
+  addButtonContainer: {
+    flex: 4,
+    padding: 0
+  },
+  addButton: {
+    backgroundColor: 'transparent',
+    elevation: 0,
+    width: 100,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  addButtonText: {
+    color: 'blue'
+  },
+
+  binsLabelView: {
+    flex: 1,
+    marginRight: 5
+  },
+  binsView: {
+    flex: 4,
+    marginBottom: 18
+  },
+  binView: {
+    paddingBottom: 4,
+    flexDirection: 'row',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  binInput: {
+    marginLeft: 8,
+    marginRight: 8,
+    flex: 1,
+    height: 30,
+    padding: 0,
+    paddingLeft: 8,
+    borderWidth: 1
+  },
+
+  binAssignmentText: {
+    flex: 1,
+    fontSize: 15,
+    marginRight: 5
+  },
+  binAssignmentInput: {
+    flex: 1,
     height: 40,
     borderWidth: 1
   }
